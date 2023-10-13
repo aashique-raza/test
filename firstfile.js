3caaa68f27250e3db3450f093e510320f93000dd
farhan ansari <farhanraza2239@gmail.com>
2023-10-13 19:52:30 +0530

    create server using express

--- firstfile.js
+++ firstfile.js
@@ -11,3 +11,8 @@ const otp=Math.floor(Math.random()*999999)
 }
 const otp=getOTP()
 console.log(otp)
+function mul(value1,value2){
+  return total=value1*value2
+}
+const mulTotal=mul(3,5);
+console.log(mulTotal)
