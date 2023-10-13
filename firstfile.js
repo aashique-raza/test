c85f11a76276ec709d95abb7d14c6375a7755c2e
farhan ansari <145009116+aashique-raza@users.noreply.github.com>
2023-10-13 18:59:36 +0530

    Update firstfile.js
    add a new function

--- firstfile.js
+++ firstfile.js
@@ -4,3 +4,10 @@ function add(a,b){
 
 const addnumber=add(2,3);
 console.log(addnumber);
+
+function getOTP(){
+const otp=Math.floor(Math.random()*999999)
+  return otp;
+}
+const otp=getOTP()
+console.log(otp)
